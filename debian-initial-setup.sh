81dfd7e18379926c5fa8da826ded41f5d93c282a
ahmad2702 <s.ahmad2702@gmail.com>
2025-12-07 17:56:34 +0100

    chore: adapt script for debian 13

--- debian-initial-setup.sh
+++ debian-initial-setup.sh
@@ -90,14 +90,14 @@ fi
 '
 if should_run "disableIPv6" "$@"; then
     echo "Disabling IPv6..."
+    touch /etc/sysctl.d/99-disable-ipv6.conf
     {
         {
             echo "net.ipv6.conf.all.disable_ipv6 = 1"
             echo "net.ipv6.conf.default.disable_ipv6 = 1"
             echo "net.ipv6.conf.lo.disable_ipv6 = 1"
-        } >> /etc/sysctl.conf
+        } >> /etc/sysctl.d/99-disable-ipv6.conf
         echo "Applying sysctl-settings..."
-        sysctl -p
     } &> /dev/null
 else
     echo "Disabling IPv6... [SKIPPED]"
